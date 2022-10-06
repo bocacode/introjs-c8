9e04fb1564c0e7e65b94e4ec28caf7cb4edc9b06
Damian Montero <photoca@hotmail.com>
2022-10-06 10:34:12 -0400

    Homework and File example from Thursday

---
+++ Homework/tree.js
@@ -0,0 +1,11 @@
+// example: 5 - "#####"
+let numberOfHashes = 5
+
+let x = "#" // # <-- concatination 
+
+
+for (let i = 1; i <= numberOfHashes; i++) {
+    console.log(x)
+    x = x + "#" // Add one more to the first 5 hash tags
+}
+
